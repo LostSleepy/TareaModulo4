27d26270af6b542a686f9634f1c24ae331e89282
Pedro Barrante Vazquez <pedroelchico0@gmail.com>
2024-10-07 13:23:06 +0200

    Subida

---
+++ NetBeansPrueba/src/main/java/com/mycompany/netbeansprueba/NetBeansPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbeansprueba;
+
+/**
+ *
+ * @author cxctxs_jxck
+ */
+public class NetBeansPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Pedro Barrante Vázquez");
+    }
+}
